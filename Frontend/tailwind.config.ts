cd768157bc6a51e1dbc4343edc7a2f32c9b10750
Deep Bodani <harshabodani461@gmail.com>
2026-01-06 16:57:34 +0530

    fix: restore styling after refactor (fix tailwind paths and plugins)

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
+import tailwindTypography from "@tailwindcss/typography";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
@@ -103,5 +105,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [tailwindAnimate, tailwindTypography],
 } satisfies Config;
